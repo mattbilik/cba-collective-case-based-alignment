97b1ed8c282c9d10db38c229901a3a233454cbcb
mbilik <mbilik@umich.edu>
2026-01-29 19:24:19 -0800

    a little bit of refactoring

--- cai_all.py
+++ cai_all.py
@@ -4,8 +4,7 @@ from transformers import BitsAndBytesConfig
 from ai_completions_hf_model_anthropic import create_revisions
 from create_sft_model import finetune_and_merge_weights
 from hf_rlaif import grpo_sft_model_with_reward_model
-
-from tests.deepeval_tests import test_deepeval_benchmarks
+from deepeval_tests import test_deepeval_benchmarks
 
 # Specify model information
 BASE_MODEL_NAME = "Qwen/Qwen2-1.5B"
@@ -35,16 +34,20 @@ bnb_config = BitsAndBytesConfig(
 
 class Config:
     bnb_config = bnb_config
+    sft_on_revisions = False
 
 if __name__ == "__main__":
     
     # Revisions and SFT only for critique + revise, we're not critiquing for CCAI
-    # create_revisions(model_name=BASE_MODEL_NAME, constitution_path='constitution_from_doc.json')
-    # finetune_and_merge_weights(config, model_name=BASE_MODEL_NAME)
     
+    if Config.sft_on_revisions:
+        create_revisions(model_name=BASE_MODEL_NAME, constitution_path='constitution_from_doc.json')
+        finetune_and_merge_weights(Config, model_name=BASE_MODEL_NAME)
+    
+    # TODO: Change this to the SFT model name if we do SFT
     sft_model_name = BASE_MODEL_NAME
     
     # The reward model's base model is the same as the SFT or non-SFT'd model that we're fine-tuning
-    final_model_name = grpo_sft_model_with_reward_model(config=Config, model_name=sft_model_name, constitution_path='constitution_from_doc.json')
+    final_model_name = grpo_sft_model_with_reward_model(Config, model_name=sft_model_name, constitution_path='constitution_from_doc.json')
     
     test_deepeval_benchmarks(final_model_name)
