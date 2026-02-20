1b3ea9ccac79dc27e112e3395d12ffd28fdf6043
mbilik <mbilik@umich.edu>
2026-02-19 18:51:58 -0800

    generate sft accelerate edits

--- accelerate_rlaif/generate_sft_dataset.py
+++ accelerate_rlaif/generate_sft_dataset.py
@@ -3,6 +3,7 @@ import json
 import random
 from hh_preferences.preference_datasets import get_pytorch_iterator
 from accelerate import Accelerator
+from accelerate.utils import gather_object
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from sentence_transformers import SentenceTransformer
 import torch
@@ -243,6 +244,18 @@ def run_generation(prompt_iterator, tokenizer, model, accelerator, constitution)
         
         responses.extend(final_completion)
         
+    gathered_data = gather_object(responses)
+    
+    if accelerator.is_main_process:
+        # NOTE: check this
+        # Flatten the list of lists into one big list
+        flat_dataset = [item for sublist in gathered_data for item in sublist]
+        
+        # Use the length of the original dataset to trim off DDP padding
+        # This replaces what gather_for_metrics does automatically for tensors
+        total_samples = len(prompt_iterator.dataset)
+        responses = flat_dataset[:total_samples]
+
     return responses
 
 def prompt_from_hh_anthropic(instruction):
