16c4b7e7a90c82cd80870f99f76854cc410c98c6
mbilik <mbilik@umich.edu>
2026-02-25 18:28:25 -0800

    added testing logic for base model

--- accelerate_rlaif/testing_suite/mmlu.py
+++ accelerate_rlaif/testing_suite/mmlu.py
@@ -136,17 +136,26 @@ def test_deepeval_benchmarks(model_name, base_model_name) -> int:
     
     model_to_test = CustomRLAIFModel(model_name, base_model_name)            
     model_to_test = LoggingModel(model_to_test)
+    
+    base_model = CustomRLAIFModel(base_model_name, base_model_name)
 
     mmlu_benchmark = MMLU(
         tasks=[MMLUTask.HIGH_SCHOOL_MATHEMATICS],
         n_shots=5
     )
     
+    print("Evaluating model to test")
     mmlu_benchmark.evaluate(model=model_to_test, run_async=True)
     
     print("MMLU Benchmark Results:")
     print(mmlu_benchmark.overall_score)
+
+    print("Evaluating base model")
+    mmlu_benchmark.evaluate(model=base_model, run_async=True)
     
+    print("MMLU Benchmark Results:")
+    print(mmlu_benchmark.overall_score)
+
     # for task in mmlu_benchmark.tasks:
     #     print(f"Task: {task.name}, Score: {task.score}")
 
