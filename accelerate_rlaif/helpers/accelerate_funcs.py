b3fac40239fa8ff6ebc1c025058595ab5d8473ff
Vinay Koshy <vinyoshy2@gmail.com>
2026-03-17 15:12:57 -0700

    restructured ai judge code

--- accelerate_rlaif/helpers/accelerate_funcs.py
+++ accelerate_rlaif/helpers/accelerate_funcs.py
@@ -6,22 +6,14 @@ def gather_iterator_batches(responses: list,
                             accelerator: Accelerator,
                             prompt_iterator: DataLoader):
     
-    gathered_data = gather_object(responses)
-    
-    if accelerator.is_main_process:
-        
-        # print(f"Gathered data from all processes: {gathered_data}")
-        
-        # NOTE: check this
-        # Flatten the list of lists into one big list
-        flat_dataset = [item for sublist in gathered_data for item in sublist]
-        
-        # Use the length of the original dataset to trim off DDP padding
-        # This replaces what gather_for_metrics does automatically for tensors
-        total_samples = len(prompt_iterator.dataset)
-        responses = flat_dataset[:total_samples]
-        
-        return gathered_data
-    
-    return []
-        
+    gathered_data = gather_object(responses)    
+    # NOTE: check this
+    # Flatten the list of lists into one big list
+    # I think gather_object will return something that's already flattened - Vinay
+    #flat_dataset = [item for sublist in gathered_data for item in sublist]
+    # Use the length of the original dataset to trim off DDP padding
+    # This replaces what gather_for_metrics does automatically for tensors
+    total_samples = len(prompt_iterator.dataset)
+    responses = gathered_data[:total_samples]    
+    return responses
+            
