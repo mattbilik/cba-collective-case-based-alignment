c9cef57ccf677be16ccb0f54be68c92f1685ab92
mbilik <mbilik@umich.edu>
2026-04-02 18:30:39 -0700

    added a bash file for testing all

--- run_all.sh
+++ run_all.sh
@@ -5,14 +5,21 @@ NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
 MODEL="Qwen/Qwen2-0.5B"
 TRAINING_SIZE="$1"
 
-echo "Beginning to train with $NUM_GPUS GPUs with $TRAINING_SIZE training examples"
+echo "Beginning to train $MODEL with $NUM_GPUS GPUs ($TRAINING_SIZE training examples)"
 echo "------------------------------------"
-echo "\nCreating $TRAINING_SIZE datapoints for GRPO"
+echo "\nCreating $TRAINING_SIZE datapoints for GRPO:"
 accelerate launch --multi_gpu --num_processes $NUM_GPUS accelerate_rlaif/generate_grpo_dataset.py $MODEL constitution_from_doc.json "$TRAINING_SIZE" n
-echo "\nTraining the reward model"
+echo "\nTraining the reward model:"
 accelerate launch --multi_gpu --num_processes $NUM_GPUS accelerate_rlaif/training_reward_model.py dataset.json models/final_reward_model $MODEL
-echo "\nTraining the policy model with the reward model"
+echo "\nTraining the policy model with the reward model:"
 accelerate launch --multi_gpu --num_processes $NUM_GPUS accelerate_rlaif/training_grpo_with_reward_model.py dataset.json models/final_reward_model $MODEL models/grpo_checkpoints models/grpo_model_constitution_FINAL
 
+# After training, create the figures for the training logs:
+echo "------------------------------------"
+echo "\nCreating figures for training logs:"
+python accelerate_rlaif/create_figures.py models/grpo_checkpoints/grpo_training_log.json models/reward_model_constitution_checkpoints/training_reward_log.json figures
+
 # Run the tests:
+echo "------------------------------------"
+echo "\nRunning tests on the trained model:"
 ./test_all.sh $MODEL
